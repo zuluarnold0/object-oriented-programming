7f71b2799c30e7288c23e35d2d40eef7dd12fbdf
zuluarnold0 <zuluarnold0@gmail.com>
2022-04-25 12:50:50 +0200

    log results added to primitive - reference file

--- primitives-and-objects.js
+++ primitives-and-objects.js
@@ -6,14 +6,14 @@ function increaseAge1(age1) {
 }
 
 increaseAge1(age1)
-console.log('Age1: ', age1)
+console.log('Age1: ', age1) //Age1: 5
 
 /**objects copies by reference */
-let ageTwo = { value: 10 }
+let ageTwo = { value: 5 }
 
 function increaseAgeTwo(ageTwo) {
   ageTwo.value++
 }
 
 increaseAgeTwo(ageTwo)
-console.log('AgeTwo: ', ageTwo)
+console.log('AgeTwo: ', ageTwo) //AgeTwo: 6
