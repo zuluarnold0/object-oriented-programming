070d85c5ff634d7858f869c1b167fb43092aea0e
zuluarnold0 <zuluarnold0@gmail.com>
2022-04-25 13:22:33 +0200

    enumeration added to object file

--- object.js
+++ object.js
@@ -1,11 +1,11 @@
 /**Object literals is a simple javascript object with properties and methods */
 
 const user = {
-  /** properties */
+  /** PROPERTIES */
   id: 1,
   name: 'star',
   surname: 'arnold',
-  /** methods */
+  /** METHODS */
   getFullName: function () {
     return this.name + ' ' + this.surname
   },
@@ -16,10 +16,34 @@ const user = {
 // console.log('surname: ', user.surname)
 // console.log('Full Name: ', user.getFullName())
 
-/** Adding properties */
+/** ADDING PROPERTIES */
 user.age = 3 //or user['age'] = 3
 // console.log(user)
 
-/** Deleting properties */
+/** DELETING PROPERTIES */
 delete user.surname // or delete user['surname']
 // console.log(user)
+
+/** LOOPING THOUGH OBJECTS: ENUMERATION */
+for (item in user) {
+  /** PRINTING KEYS */
+  // console.log(item)
+  /** PRINTING VALUES */
+  // console.log(user[item])
+}
+
+/** FINDING KEYS USING A METHOD */
+const keys = Object.keys(user)
+// console.log(keys)
+
+/** GETTING PROPERTIES WITH A PARTICULAR TYPE */
+for (item in user) {
+  if (typeof user[item] === 'number') {
+    // console.log(item)
+  }
+}
+
+/** CHECKING IF A PROPERTY IS IN AN OBJECT */
+if ('age' in user) {
+  console.log('found age')
+}
